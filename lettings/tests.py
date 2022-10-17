70ef0272fcdaab88b779ea68a117c1d8808e8622
Alexy Dubois <alexy.dubois62@gmail.com>
2022-10-17 19:41:17 +0200

    update tests

--- lettings/tests.py
+++ lettings/tests.py
@@ -5,31 +5,30 @@ from django.urls import reverse
 
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_letting(client):
-    address = Address.objects.create(number=1007,
-                                     street='Mountain Drive',
-                                     city='Gotham City',
-                                     state='NJ',
+    address = Address.objects.create(number=1,
+                                     street='test-street',
+                                     city='test-city',
+                                     state='test-state',
                                      zip_code='12345',
-                                     country_iso_code='USA'
+                                     country_iso_code='FR'
                                      )
-    Letting.objects.create(title="Wayne Manor",
-                           address_id=address.id)
+    Letting.objects.create(title="test-title", address_id=address.id)
     response = client.get(reverse("lettings_index"))
 
     assert response.status_code == 200
-    assert b"Wayne Manor" in response.content
+    assert b"test-title" in response.content
 
 
 @pytest.mark.django_db(transaction=True, reset_sequences=True)
 def test_letting_detail(client):
-    address = Address.objects.create(number=1007,
-                                     street='Mountain Drive',
-                                     city='Gotham City',
-                                     state='NJ',
+    address = Address.objects.create(number=1,
+                                     street='test-street',
+                                     city='test-city',
+                                     state='test-state',
                                      zip_code='12345',
-                                     country_iso_code='USA'
+                                     country_iso_code='FR'
                                      )
-    letting = Letting.objects.create(title="Wayne Manor", address_id=address.id)
+    letting = Letting.objects.create(title="test-title", address_id=address.id)
     response = client.get(reverse("letting", kwargs={"letting_id": letting.id}))
 
-    assert b"Mountain Drive" in response.content
+    assert b"test-street" in response.content
--- profiles/tests.py
+++ profiles/tests.py
@@ -6,31 +6,31 @@ from django.urls import reverse
 User = get_user_model()
 @pytest.mark.django_db
 def test_profile(client):
-    user = User.objects.create(username='Batman',
-                               password='jokerwillpay4it',
-                               first_name='Bruce',
-                               last_name='Wayne',
-                               email='bruce.wayne@')
+    user = User.objects.create(username='test-username',
+                               password='test-password',
+                               first_name='test-first_name',
+                               last_name='test-last_name',
+                               email='test-email')
     Profile.objects.create(user_id=user.id,
-                           favorite_city='Paris'
+                           favorite_city='test-favorite_city'
                            )
 
     response = client.get(reverse("profiles_index"))
 
     assert response.status_code == 200
-    assert b"Batman" in response.content
+    assert b"test-username" in response.content
 
 
 @pytest.mark.django_db
 def test_profile_detail(client):
-    user = User.objects.create(username='Batman',
-                               password='jokerwillpay4it',
-                               first_name='Bruce',
-                               last_name='Wayne',
-                               email='bruce.wayne@')
+    user = User.objects.create(username='test-username',
+                               password='test-password',
+                               first_name='test-first_name',
+                               last_name='test-last_name',
+                               email='test-email')
     Profile.objects.create(user_id=user.id,
-                           favorite_city='Paris'
+                           favorite_city='test-favorite_city'
                            )
     response = client.get(reverse("profile", kwargs={"username": user.username}))
 
-    assert b"Paris" in response.content
+    assert b"test-favorite_city" in response.content
